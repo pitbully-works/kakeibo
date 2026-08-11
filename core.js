abc2dd9936376dfae0f459577fdc88dbf676e0a3
pitbully-works <kunihiko.hioki@icloud.com>
2026-08-11 17:41:44 +0900

    Add files via upload

--- core.js
+++ core.js
@@ -30,7 +30,7 @@
 
   /* 画面の「アプリ情報」に出す版数。上げるときはここだけを書き換える。
      （service worker のキャッシュ名 kakeibo-vNN とは別のもの） */
-  const APP_VERSION = "1.12.4";
+  const APP_VERSION = "1.12.5";
 
   /* ---------- 分類の定義 ---------- */
 
